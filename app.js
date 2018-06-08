0d5f10e4e569f6453183f379998e061a7680c3de
Lugas Septiawan <lugassawan@gmail.com>
2018-06-08 16:31:13 +0700

    getting API data

---
+++ app.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBfAIoHJtVoMMleBNskCHCNNalsORtl-q8`,
+    json: true
+}, (error, response, body) => {
+    //console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`lat: ${body.results[0].geometry.location.lat}`);
+    console.log(`lng: ${body.results[0].geometry.location.lng}`);
+});
